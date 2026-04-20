37cb874bc8d21e372162a4afd4b39a617165f2d0
anshverma1975 <astrophile.ansh@gmail.com>
2026-04-20 16:07:54 +0530

    v1.4

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for , redirect, flash, session
 from controller.config import Config
 from controller.database import db
-from controller.models import User, Subject, Quiz, Question, Attempt
+from controller.models import User, Subject, Quiz, Question, Attempt, Activity
 
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
@@ -43,8 +43,17 @@ def home():
     questions_count = Question.query.count()
     attempted_count = Attempt.query.filter_by(user_id=user_id).count()
 
+    activities = (
+        Activity.query
+        .filter_by(user_id=user_id)
+        .order_by(Activity.timestamp.desc())
+        .limit(5)
+        .all()
+    )
+
+
     return render_template(
-        "user_dashboard.html",subjects_count=subjects_count, quizzes_count=quizzes_count, questions_count=questions_count, attempted_count=attempted_count
+        "user_dashboard.html",subjects_count=subjects_count, quizzes_count=quizzes_count, questions_count=questions_count, attempted_count=attempted_count,activities=activities   
     )
 
 
@@ -93,6 +102,7 @@ def register():
         new_user = User(username=username, password=password, role="student")
         db.session.add(new_user)
         db.session.commit()
+        log_activity(type="system",message=f"User {username} Registered",user_id=new_user.id)
 
         flash("Registered successfully, please login")
         return redirect(url_for('login'))
@@ -108,6 +118,15 @@ def logout():
     flash("Logged out successfully")
     return redirect(url_for('landing'))
 
+def log_activity(type, message, user_id=None):
+    activity = Activity(
+        type=type,
+        message=message,
+        user_id=user_id
+    )
+    db.session.add(activity)
+    db.session.commit()
+
 @app.route('/admin')
 def admin_dashboard():
     if "user_id" not in session:
@@ -116,8 +135,16 @@ def admin_dashboard():
     if session.get('role') != "admin":
         flash("You are not authorized to access this page")
         return redirect(url_for("landing"))
-    return render_template("admin_dashboard.html")
+    
+    users_count = User.query.count()
+    subjects_count = Subject.query.count()
+    quizzes_count = Quiz.query.count()
+    questions_count = Question.query.count()
+    activities = (Activity.query.order_by(Activity.timestamp.desc()).limit(5).all())
+    
+
+    return render_template("admin_dashboard.html",users_count=users_count,subjects_count=subjects_count,quizzes_count=quizzes_count,questions_count=questions_count,activities=activities)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
