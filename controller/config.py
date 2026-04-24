217ccf4128f0c10713ac7110b20d90714ab8531f
anshverma1975 <astrophile.ansh@gmail.com>
2026-04-24 20:32:32 +0530

    migration

--- controller/config.py
+++ controller/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = "mere_desh_ki_dhartiiiiiiii"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///quiz_master.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///quiz_master.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
