2830cadeef26c5d28e1b10475c705a29baaefceb
anshverma1975 <astrophile.ansh@gmail.com>
2026-04-25 08:07:18 +0530

    Switch to PostgreSQL

--- controller/config.py
+++ controller/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "mere_desh_ki_dhartiiiiiiii"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "mere_desh_ki_dhartiiiiiiii")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///quiz_master.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
